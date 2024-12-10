9b06e1800546a3b4c16a2d3241031bcb7b7e6f00
Ahmed-Abouzaid <ahmed.saber.pro@gmail.com>
2024-12-10 22:34:39 +0200

    assets file

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,177 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for fontsCairoCairoBlack
+  /// assets/fonts/Cairo/Cairo-Black.ttf
+  static const String fontsCairoCairoBlack = "assets/fonts/Cairo/Cairo-Black.ttf";
+
+  /// Assets for fontsCairoCairoBold
+  /// assets/fonts/Cairo/Cairo-Bold.ttf
+  static const String fontsCairoCairoBold = "assets/fonts/Cairo/Cairo-Bold.ttf";
+
+  /// Assets for fontsCairoCairoExtraLight
+  /// assets/fonts/Cairo/Cairo-ExtraLight.ttf
+  static const String fontsCairoCairoExtraLight = "assets/fonts/Cairo/Cairo-ExtraLight.ttf";
+
+  /// Assets for fontsCairoCairoLight
+  /// assets/fonts/Cairo/Cairo-Light.ttf
+  static const String fontsCairoCairoLight = "assets/fonts/Cairo/Cairo-Light.ttf";
+
+  /// Assets for fontsCairoCairoRegular
+  /// assets/fonts/Cairo/Cairo-Regular.ttf
+  static const String fontsCairoCairoRegular = "assets/fonts/Cairo/Cairo-Regular.ttf";
+
+  /// Assets for fontsCairoCairoSemiBold
+  /// assets/fonts/Cairo/Cairo-SemiBold.ttf
+  static const String fontsCairoCairoSemiBold = "assets/fonts/Cairo/Cairo-SemiBold.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayBlack
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-Black.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayBlack = "assets/fonts/PlayfairDisplay/PlayfairDisplay-Black.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayBlackItalic
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-BlackItalic.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayBlackItalic = "assets/fonts/PlayfairDisplay/PlayfairDisplay-BlackItalic.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayBold
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-Bold.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayBold = "assets/fonts/PlayfairDisplay/PlayfairDisplay-Bold.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayBoldItalic
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-BoldItalic.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayBoldItalic = "assets/fonts/PlayfairDisplay/PlayfairDisplay-BoldItalic.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayExtraBold
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-ExtraBold.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayExtraBold = "assets/fonts/PlayfairDisplay/PlayfairDisplay-ExtraBold.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayExtraBoldItalic
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-ExtraBoldItalic.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayExtraBoldItalic = "assets/fonts/PlayfairDisplay/PlayfairDisplay-ExtraBoldItalic.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayItalic
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-Italic.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayItalic = "assets/fonts/PlayfairDisplay/PlayfairDisplay-Italic.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayMedium
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-Medium.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayMedium = "assets/fonts/PlayfairDisplay/PlayfairDisplay-Medium.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayMediumItalic
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-MediumItalic.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayMediumItalic = "assets/fonts/PlayfairDisplay/PlayfairDisplay-MediumItalic.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplayRegular
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-Regular.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplayRegular = "assets/fonts/PlayfairDisplay/PlayfairDisplay-Regular.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplaySemiBold
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-SemiBold.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplaySemiBold = "assets/fonts/PlayfairDisplay/PlayfairDisplay-SemiBold.ttf";
+
+  /// Assets for fontsPlayfairDisplayPlayfairDisplaySemiBoldItalic
+  /// assets/fonts/PlayfairDisplay/PlayfairDisplay-SemiBoldItalic.ttf
+  static const String fontsPlayfairDisplayPlayfairDisplaySemiBoldItalic = "assets/fonts/PlayfairDisplay/PlayfairDisplay-SemiBoldItalic.ttf";
+
+  /// Assets for images001Sale
+  /// assets/images/001-sale.png
+  static const String images001Sale = "assets/images/001-sale.png";
+
+  /// Assets for images002DeliveryTruck
+  /// assets/images/002-delivery-truck.png
+  static const String images002DeliveryTruck = "assets/images/002-delivery-truck.png";
+
+  /// Assets for images002SaleTag
+  /// assets/images/002-sale-tag.png
+  static const String images002SaleTag = "assets/images/002-sale-tag.png";
+
+  /// Assets for images003CashOnDelivery
+  /// assets/images/003-cash-on-delivery.png
+  static const String images003CashOnDelivery = "assets/images/003-cash-on-delivery.png";
+
+  /// Assets for images003Sale1
+  /// assets/images/003-sale-1.png
+  static const String images003Sale1 = "assets/images/003-sale-1.png";
+
+  /// Assets for images004Box
+  /// assets/images/004-box.png
+  static const String images004Box = "assets/images/004-box.png";
+
+  /// Assets for images005DeliveryMan
+  /// assets/images/005-delivery-man.png
+  static const String images005DeliveryMan = "assets/images/005-delivery-man.png";
+
+  /// Assets for images006Delivery
+  /// assets/images/006-delivery.png
+  static const String images006Delivery = "assets/images/006-delivery.png";
+
+  /// Assets for images1
+  /// assets/images/1.png
+  static const String images1 = "assets/images/1.png";
+
+  /// Assets for images3
+  /// assets/images/3.png
+  static const String images3 = "assets/images/3.png";
+
+  /// Assets for images5
+  /// assets/images/5.png
+  static const String images5 = "assets/images/5.png";
+
+  /// Assets for imagesAvatar
+  /// assets/images/avatar.png
+  static const String imagesAvatar = "assets/images/avatar.png";
+
+  /// Assets for imagesDrivethru
+  /// assets/images/drivethru.png
+  static const String imagesDrivethru = "assets/images/drivethru.png";
+
+  /// Assets for imagesLaptop
+  /// assets/images/laptop.png
+  static const String imagesLaptop = "assets/images/laptop.png";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.png
+  static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesLogoapp
+  /// assets/images/logoapp.png
+  static const String imagesLogoapp = "assets/images/logoapp.png";
+
+  /// Assets for imagesNodata
+  /// assets/images/nodata.gif
+  static const String imagesNodata = "assets/images/nodata.gif";
+
+  /// Assets for imagesOne
+  /// assets/images/one.PNG
+  static const String imagesOne = "assets/images/one.PNG";
+
+  /// Assets for imagesThree
+  /// assets/images/three.PNG
+  static const String imagesThree = "assets/images/three.PNG";
+
+  /// Assets for imagesTwo
+  /// assets/images/two.PNG
+  static const String imagesTwo = "assets/images/two.PNG";
+
+  /// Assets for lottieCart
+  /// assets/lottie/cart.json
+  static const String lottieCart = "assets/lottie/cart.json";
+
+  /// Assets for lottieEcommerce
+  /// assets/lottie/ecommerce.json
+  static const String lottieEcommerce = "assets/lottie/ecommerce.json";
+
+  /// Assets for lottieNodata
+  /// assets/lottie/nodata.json
+  static const String lottieNodata = "assets/lottie/nodata.json";
+
+  /// Assets for lottieOffline
+  /// assets/lottie/offline.json
+  static const String lottieOffline = "assets/lottie/offline.json";
+
+  /// Assets for lottieServer
+  /// assets/lottie/server.json
+  static const String lottieServer = "assets/lottie/server.json";
+}
+
---
+++ lib/data/source/static.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerce/core/constants/assets.dart';
+import 'package:ecommerce/data/model/onboardingmodel.dart';
+
+List<OnBoardingModel> OnBoardingList = [
+  OnBoardingModel(
+    title: "Choose Product",
+    body:
+        " We have a 100k Product, Choose \n Your Product from our E-commerce shop",
+    image: Assets.imagesOne,
+  ),
+  OnBoardingModel(
+    title: "Choose Product",
+    body:
+        " We have a 100k Product, Choose \n Your Product from our E-commerce shop",
+    image: Assets.imagesOne,
+  ),
+  OnBoardingModel(
+    title: "Choose Product",
+    body:
+        " We have a 100k Product, Choose \n Your Product from our E-commerce shop",
+    image: Assets.imagesOne,
+  ),
+  OnBoardingModel(
+    title: "Choose Product",
+    body:
+        " We have a 100k Product, Choose \n Your Product from our E-commerce shop",
+    image: Assets.imagesOne,
+  ),
+];
